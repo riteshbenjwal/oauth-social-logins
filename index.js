959aa3844450cbf2de7f6000dcdb3d246e760309
Ritesh Benjwal <riteshbenjwal7@gmail.com>
2022-03-28 19:52:37 +0530

    BASIC_STRUCTURE

--- index.js
+++ index.js
@@ -1,1 +1,21 @@
-hello 
+const express = require("express");
+const mongoose = require('mongoose');
+
+//connect to db
+
+mongoose.connect('mongodb://127.0.0.1:27017/passport',()=>{
+    console.log("DB CONNECTED")
+});
+
+const app = express();
+
+app.set('view engine', 'ejs');
+
+
+app.get('/', (req,res)=>{
+  res.render("home");
+})
+
+app.listen(4000, () => {
+  console.log(`Server is running at port 4000..`);
+});
