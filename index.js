46e1d0aa44253f654800a19d92e4be87efe739ea
Ritesh Benjwal <riteshbenjwal7@gmail.com>
2022-03-29 08:09:59 +0530

    Completed

--- index.js
+++ index.js
@@ -1,24 +1,40 @@
 const express = require("express");
-const mongoose = require('mongoose');
-const auth = require("./routes/auth")
+const mongoose = require("mongoose");
+const auth = require("./routes/auth");
+const passportConfig = require("./passport/passport");
+const passport = require("passport");
+const cookieSession = require("cookie-session");
 const app = express();
-
-//connect to db
-
-mongoose.connect('mongodb://127.0.0.1:27017/passport',()=>{
-    console.log("DB CONNECTED")
-});
+require("dotenv").config();
 
 
-app.set('view engine', 'ejs');
-app.use("/auth",auth);
+//connect to the DB
+mongoose.connect("mongodb://127.0.0.1:27017/passport", () =>
+  console.log("DB CONNECTED")
+);
+
+app.use(cookieSession({
+  maxAge: 1 * 24 * 60 * 60 * 1000,
+  keys: [process.env.KEYS]
+}))
 
 
+app.use(passport.initialize());
+app.use(passport.session());
 
-app.get('/', (req,res)=>{
+
+const isLoggedIn = (req, res, next) => {
+  if (!req.user) {
+    res.redirect("/auth/login");
+  }
+  next();
+};
+
+app.set("view engine", "ejs");
+app.use("/auth", auth);
+
+app.get("/", isLoggedIn, (req, res) => {
   res.render("home");
-})
-
-app.listen(4000, () => {
-  console.log(`Server is running at port 4000..`);
 });
+
+app.listen(4000, () => console.log(`Server is running at port 4000...`));
--- passport/passport.js
+++ passport/passport.js
@@ -1,13 +1,48 @@
-const passport = require('passport');
+const passport = require("passport");
+const User = require("../model/user");
+require("dotenv").config();
 
 let GoogleStrategy = require("passport-google-oauth20").Strategy;
 
-passport.use(new GoogleStrategy(
-   {
-    clientID:  "761347088789-rllmfv9jd0p99g814h0n5o15r4kac92t.apps.googleusercontent.com",
-    clientSecret: "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",
-   },
-   ()=>{
-       
-   }
-))
+passport.serializeUser(function (user, done) {
+  done(null, user.id);
+});
+
+passport.deserializeUser(function (id, done) {
+  User.findById(id, function (err, user) {
+    done(err, user);
+  });
+});
+
+passport.use(
+  new GoogleStrategy(
+    {
+      clientID: process.env.CLIENTID,
+      clientSecret: process.env.CLIENTSECRET,
+      callbackURL: "http://localhost:4000/auth/google/callback",
+    },
+    (accessToken, refreshToken, profile, next) => {
+      console.log("MY PROFILE", profile._json.email);
+      User.findOne({ email: profile._json.email }).then((user) => {
+        if (user) {
+          console.log("User already exits in DB", user);
+          next(null, user);
+          // cookietoken()
+        } else {
+          User.create({
+            name: profile.displayName,
+            googleId: profile.id,
+            email: profile._json.email,
+          })
+            .then((user) => {
+              console.log("New User", user);
+              next(null, user);
+              // cookietoken()
+            })
+            .catch((err) => console.log(err));
+        }
+      });
+      //   next();
+    }
+  )
+);
